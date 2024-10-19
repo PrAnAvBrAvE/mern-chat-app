ad38730664260895cef037f6c8a7b8519696013c
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2024-10-19 17:02:30 +0530

    Changed URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const newSocket = io("http://localhost:5000", {
+      const newSocket = io("https://realtime-chat-app-g5h6.onrender.com", {
         query: {
             userId: authUser._id,
         }
