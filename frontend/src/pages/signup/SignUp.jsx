40d778c1230375fed87a1a43460e7ed24360167b
Pranav Kumar Patil <95088578+PrAnAvBrAvE@users.noreply.github.com>
2026-09-24 02:08:52 +0530

    Resolved placeholder issue

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -48,7 +48,7 @@ const SignUp = () => {
                         <label className='label p-2 '>
                             <span className='text-base label-text'>Username</span>
                         </label>
-                        <input type='text' placeholder='pranavpatil' autoComplete='off' className='w-full input input-bordered h-10'
+                        <input type='text' placeholder='pranav' autoComplete='off' className='w-full input input-bordered h-10'
                             value={inputs.username}
                             onChange={(e) => setInputs({...inputs, username: e.target.value})}
                         />
